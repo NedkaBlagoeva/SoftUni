cd1448d3932f41546053f8ae42c5e6302eefcbc0
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-13 16:47:10 +0300

    proba

--- Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops/_06_ForeignLanguages.java
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops/_06_ForeignLanguages.java
@@ -6,6 +6,6 @@ public class _06_ForeignLanguages {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String country = scan.nextLine();
-        
+        sfvdsvs
     }
 }
