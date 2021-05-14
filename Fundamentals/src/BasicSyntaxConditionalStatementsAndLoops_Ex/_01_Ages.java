282b23cbccc993132266b8f2fe2205490f6e9e9f
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-14 12:35:07 +0300

    Tasks from current topic

---
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_Ex/_01_Ages.java
@@ -0,0 +1,10 @@
+package BasicSyntaxConditionalStatementsAndLoops_Ex;
+
+import java.util.Scanner;
+
+public class _01_Ages {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age = Integer.parseInt(scan.nextLine());
+    }
+}
