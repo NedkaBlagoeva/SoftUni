7dc58899133134b1ddde7a88cf25e22b349c62e8
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-14 15:45:41 +0300

    Topic's tasks

--- Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_Ex/_06_StrongNumber.java
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_Ex/_06_StrongNumber.java
@@ -7,6 +7,27 @@ public class _06_StrongNumber {
         Scanner scan = new Scanner(System.in);
 
         int input = Integer.parseInt(scan.nextLine());
-        
+        int number = input;
+        int digit = 0;
+        int fact = 1;
+        int sum = 0;
+
+        while (input > 0) {
+            digit = input % 10;
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+            sum += fact;
+            fact = 1;
+            if (input > 0) {
+                input = input / 10;
+            }
+        }
+        if (sum == number) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
     }
 }
