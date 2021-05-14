9955ae583769094efe82b1b32c582600a8616960
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-14 15:03:35 +0300

    Tasks on the topic

---
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_Ex/_02_Division.java
@@ -0,0 +1,24 @@
+package BasicSyntaxConditionalStatementsAndLoops_Ex;
+
+import java.util.Scanner;
+
+public class _02_Division {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int input = Integer.parseInt(scan.nextLine());
+
+        if (input % 10 == 0) {
+            System.out.println("The number is divisible by 10");
+        } else if (input % 7 == 0) {
+            System.out.println("The number is divisible by 7");
+        } else if (input % 6 == 0) {
+            System.out.println("The number is divisible by 6");
+        } else if (input % 3 == 0) {
+            System.out.println("The number is divisible by 3");
+        } else if (input % 2 == 0) {
+            System.out.println("The number is divisible by 2");
+        } else {
+            System.out.println("Not divisible");
+        }
+    }
+}
---
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_Ex/_06_StrongNumber.java
@@ -0,0 +1,12 @@
+package BasicSyntaxConditionalStatementsAndLoops_Ex;
+
+import java.util.Scanner;
+
+public class _06_StrongNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int input = Integer.parseInt(scan.nextLine());
+        
+    }
+}
