b21508168eb28248e35fec188992c65960d469e8
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-15 20:02:00 +0300

    Update _03_GamingStore.java

--- Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_03_GamingStore.java
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_03_GamingStore.java
@@ -6,15 +6,68 @@ public class _03_GamingStore {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double balance = Double.parseDouble(scan.nextLine());
+        double money = balance;
+        String gameType = scan.nextLine();
 
-        
-        Name	Price
-        OutFall 4	$39.99
-        CS: OG	$15.99
-        Zplinter Zell	$19.99
-        Honored 2	$59.99
-        RoverWatch	$29.99
-        RoverWatch Origins Edition	$39.99
-
+        while (!gameType.equals("Game Time")) {
+            if (balance == 0) {
+                System.out.println("Out of money!");
+                break;
+            }
+            switch (gameType) {
+                case "OutFall 4":
+                    if (balance >= 39.99) {
+                        balance -= 39.99;
+                        System.out.println("Bought OutFall 4");
+                    } else {
+                        System.out.println("Too Expensive");
+                    }
+                    break;
+                case "CS: OG":
+                    if (balance >= 15.99) {
+                        balance -= 15.99;
+                        System.out.println("Bought CS: OG");
+                    } else {
+                        System.out.println("Too Expensive");
+                    }
+                    break;
+                case "Zplinter Zell":
+                    if (balance >= 19.99) {
+                        balance -= 19.99;
+                        System.out.println("Bought Zplinter Zell");
+                    } else {
+                        System.out.println("Too Expensive");
+                    }
+                    break;
+                case "Honored 2":
+                    if (balance >= 59.99) {
+                        balance -= 59.99;
+                        System.out.println("Bought Honored 2");
+                    } else {
+                        System.out.println("Too Expensive");
+                    }
+                    break;
+                case "RoverWatch":
+                    if (balance >= 29.99) {
+                        balance -= 29.99;
+                        System.out.println("Bought RoverWatch");
+                    } else {
+                        System.out.println("Too Expensive");
+                    }
+                    break;
+                case "RoverWatch Origins Edition":
+                    if (balance >= 39.99) {
+                        balance -= 39.99;
+                        System.out.println("Bought RoverWatch Origins Edition");
+                    } else {
+                        System.out.println("Too Expensive");
+                    }
+                    break;
+                default:
+                    System.out.println("Not Found");
+            }
+            gameType = scan.nextLine();
+        }
+        System.out.printf("Total spent: $%.2f. Remaining: $%.2f", money - balance, balance);
     }
 }
