587232a48db57527341c136e4fe08ce4841c2764
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-15 19:41:44 +0300

    Update _03_GamingStore.java

--- Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_03_GamingStore.java
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_03_GamingStore.java
@@ -1,4 +1,20 @@
 package BasicSyntaxConditionalStatementsAndLoops_MoreExercise;
 
+import java.util.Scanner;
+
 public class _03_GamingStore {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double balance = Double.parseDouble(scan.nextLine());
+
+        
+        Name	Price
+        OutFall 4	$39.99
+        CS: OG	$15.99
+        Zplinter Zell	$19.99
+        Honored 2	$59.99
+        RoverWatch	$29.99
+        RoverWatch Origins Edition	$39.99
+
+    }
 }
