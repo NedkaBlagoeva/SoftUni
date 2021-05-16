a6b56fd98a33c6a6fd606cea7e6f52bf91462305
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-16 11:15:42 +0300

    Update _04_ReverseString.java

--- Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_04_ReverseString.java
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_04_ReverseString.java
@@ -1,7 +1,16 @@
 package BasicSyntaxConditionalStatementsAndLoops_MoreExercise;
 
+import java.util.Scanner;
+
 public class _04_ReverseString {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+        String backwards = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            backwards += word.charAt(i);
+        }
+        System.out.println(backwards);
     }
 }
