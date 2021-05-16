3d3fe2575ef078744b07c3691922b2e3f197ab0c
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-16 11:18:13 +0300

    Create _05_Messages.java

---
+++ Fundamentals/src/BasicSyntaxConditionalStatementsAndLoops_MoreExercise/_05_Messages.java
@@ -0,0 +1,10 @@
+package BasicSyntaxConditionalStatementsAndLoops_MoreExercise;
+
+import java.util.Scanner;
+
+public class _05_Messages {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
