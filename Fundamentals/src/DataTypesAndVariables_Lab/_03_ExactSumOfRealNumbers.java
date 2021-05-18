efb8bec48b11fe3707669c6eabe21d258b869fd1
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-18 23:43:15 +0300

    Update _03_ExactSumOfRealNumbers.java

--- Fundamentals/src/DataTypesAndVariables_Lab/_03_ExactSumOfRealNumbers.java
+++ Fundamentals/src/DataTypesAndVariables_Lab/_03_ExactSumOfRealNumbers.java
@@ -7,11 +7,11 @@ public class _03_ExactSumOfRealNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        BigDecimal sum;
+        BigDecimal sum = BigDecimal.ZERO;
         for (int i = 1; i <= n; i++) {
             String input = scan.nextLine();
             BigDecimal number = new BigDecimal(input);
-            sum = sum.add(number);
+            sum = number.add(sum);
 
         }
         System.out.println(sum);
