2253e99c0623e5d14c6fb0985473f5d2d0729516
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-18 23:55:27 +0300

    Update _04_TownInfo.java

--- Fundamentals/src/DataTypesAndVariables_Lab/_04_TownInfo.java
+++ Fundamentals/src/DataTypesAndVariables_Lab/_04_TownInfo.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class _04_TownInfo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        String town = scan.nextLine();
+        int population = Integer.parseInt(scan.nextLine());
+        double area = Double.parseDouble(scan.nextLine());
+        System.out.printf("Town %s has population of %d and area %.0f square km.", town, population, area);
     }
 }
