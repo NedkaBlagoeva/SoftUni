61116f9402e14ba6af64ac27830d353472802819
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-18 23:43:00 +0300

    Create _04_TownInfo.java

---
+++ Fundamentals/src/DataTypesAndVariables_Lab/_04_TownInfo.java
@@ -0,0 +1,10 @@
+package DataTypesAndVariables_Lab;
+
+import java.util.Scanner;
+
+public class _04_TownInfo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
