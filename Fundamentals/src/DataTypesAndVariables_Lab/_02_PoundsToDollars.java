0f20e80a70a1c413f79d059d22a8ccdc8dba74ab
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-18 20:09:09 +0300

    Update _02_PoundsToDollars.java

--- Fundamentals/src/DataTypesAndVariables_Lab/_02_PoundsToDollars.java
+++ Fundamentals/src/DataTypesAndVariables_Lab/_02_PoundsToDollars.java
@@ -1,4 +1,13 @@
 package DataTypesAndVariables_Lab;
 
+import java.util.Scanner;
+
 public class _02_PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double poundBritish = Double.parseDouble(scan.nextLine());
+
+        System.out.printf("%.3f", poundBritish * 1.31);
+    }
 }
