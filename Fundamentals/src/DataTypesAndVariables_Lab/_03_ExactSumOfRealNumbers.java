d42907e98a353de700c27bec6504895a7ae803d0
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-18 20:16:57 +0300

    Update _03_ExactSumOfRealNumbers.java

--- Fundamentals/src/DataTypesAndVariables_Lab/_03_ExactSumOfRealNumbers.java
+++ Fundamentals/src/DataTypesAndVariables_Lab/_03_ExactSumOfRealNumbers.java
@@ -1,4 +1,12 @@
 package DataTypesAndVariables_Lab;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class _03_ExactSumOfRealNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigDecimal n = Double.parseDouble(scan.nextLine());
+        while ()
+    }
 }
