47184a43cca66229e2624487fc6deca728f9fbf2
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-20 19:27:18 +0300

    Update _08_LowerOrUpper.java

--- Fundamentals/src/DataTypesAndVariables_Lab/_08_LowerOrUpper.java
+++ Fundamentals/src/DataTypesAndVariables_Lab/_08_LowerOrUpper.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class _08_LowerOrUpper {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        char a = scan.next().charAt(0);
+        char letter = scan.next().charAt(0);
+        int a = letter;
+        if (a >= 97 && a <= )
 
-        System.out.printf("%d", a);
+            System.out.print(a);
 
 
     }
