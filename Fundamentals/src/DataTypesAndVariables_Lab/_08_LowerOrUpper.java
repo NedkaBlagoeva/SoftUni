d30890357b06a40c57d8edd476c34130de4dcd55
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-20 09:28:29 +0300

    Create _08_LowerOrUpper.java

---
+++ Fundamentals/src/DataTypesAndVariables_Lab/_08_LowerOrUpper.java
@@ -0,0 +1,14 @@
+package DataTypesAndVariables_Lab;
+
+import java.util.Scanner;
+
+public class _08_LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char a = scan.next().charAt(0);
+
+        System.out.printf("%d", a);
+
+
+    }
+}
