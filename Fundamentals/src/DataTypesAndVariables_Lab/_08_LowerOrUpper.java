adccc80ab50891a7fe2dc4afefd057e355b5e5a4
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-23 17:22:58 +0300

    Update _08_LowerOrUpper.java

--- Fundamentals/src/DataTypesAndVariables_Lab/_08_LowerOrUpper.java
+++ Fundamentals/src/DataTypesAndVariables_Lab/_08_LowerOrUpper.java
@@ -7,10 +7,9 @@ public class _08_LowerOrUpper {
         Scanner scan = new Scanner(System.in);
         char letter = scan.next().charAt(0);
         int a = letter;
-        if (a >= 97 && a <= )
-
-            System.out.print(a);
-
-
+        if (a >= 97 && a <= 122) {
+            System.out.println("lower-case");
+        } else if (a >= 65 && a <= 90)
+            System.out.println("upper-case");
     }
 }
