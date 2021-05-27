fa1d25f5b6740e4f09064c9ecc78a6ad22aaa34d
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-27 13:29:05 +0300

    Update _03_Elevator.java

--- Fundamentals/src/DataTypesAndVariables_Ex/_03_Elevator.java
+++ Fundamentals/src/DataTypesAndVariables_Ex/_03_Elevator.java
@@ -1,4 +1,20 @@
 package DataTypesAndVariables_Ex;
 
+import java.util.Scanner;
+
 public class _03_Elevator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //the number of people n and the capacity p
+        int people = Integer.parseInt(scan.nextLine());
+        int capacity = Integer.parseInt(scan.nextLine());
+        int courses = 0;
+        if (people % capacity != 0) {
+            courses = people / capacity + 1;
+        } else {
+            courses = people / capacity;
+        }
+
+        System.out.println(courses);
+    }
 }
