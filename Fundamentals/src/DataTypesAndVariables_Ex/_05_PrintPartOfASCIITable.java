490bea97266d1636c80d0619528f61df5a9ce13b
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-27 14:05:03 +0300

    Update _05_PrintPartOfASCIITable.java

--- Fundamentals/src/DataTypesAndVariables_Ex/_05_PrintPartOfASCIITable.java
+++ Fundamentals/src/DataTypesAndVariables_Ex/_05_PrintPartOfASCIITable.java
@@ -1,4 +1,17 @@
 package DataTypesAndVariables_Ex;
 
+import java.util.Scanner;
+
 public class _05_PrintPartOfASCIITable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int start = Integer.parseInt(scan.nextLine());
+        int end = Integer.parseInt(scan.nextLine());
+        int sum = 0;
+
+        for (int i = start; i <= end ; i++) {
+           char symbol = (char) i;
+            System.out.print(symbol + " ");
+        }
+    }
 }
