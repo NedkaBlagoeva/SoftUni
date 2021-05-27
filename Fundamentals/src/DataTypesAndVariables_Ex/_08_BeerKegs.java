0937d9e3dfd1a711661d20b1615add21a48be50f
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-27 19:30:08 +0300

    Update _08_BeerKegs.java

--- Fundamentals/src/DataTypesAndVariables_Ex/_08_BeerKegs.java
+++ Fundamentals/src/DataTypesAndVariables_Ex/_08_BeerKegs.java
@@ -1,4 +1,26 @@
 package DataTypesAndVariables_Ex;
 
+import java.util.Scanner;
+
 public class _08_BeerKegs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        double maxVolume = Double.MIN_VALUE;
+        double volume = 0;
+        String biggestKegModel = "";
+
+        for (int i = 1; i <= n; i++) {
+            String model = scan.nextLine();
+            float radius = Float.parseFloat(scan.nextLine());
+            int height = Integer.parseInt(scan.nextLine());
+
+            volume = Math.PI * radius * radius * height;
+            if (volume >= maxVolume) {
+                maxVolume = volume;
+                biggestKegModel = model;
+            }
+        }
+        System.out.printf("%s", biggestKegModel);
+    }
 }
