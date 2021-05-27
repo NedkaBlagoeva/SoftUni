126c2aede5f3381d1a83e956d591075798199bc2
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-27 15:37:22 +0300

    Demo

---
+++ Fundamentals/src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char s = '\u2744';
+        char a = 'N';
+        char b = 'e';
+        char c = 'D';
+        String ff = "" + s + a + b + c + '\u2744';
+        System.out.println(ff);
+
+    }
+}
