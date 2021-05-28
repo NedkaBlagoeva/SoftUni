4fb9c29bd410edf056ca4e9a4d5861d60b3e0c80
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-28 19:48:03 +0300

    Update _10_PokeMon.java

--- Fundamentals/src/DataTypesAndVariables_Ex/_10_PokeMon.java
+++ Fundamentals/src/DataTypesAndVariables_Ex/_10_PokeMon.java
@@ -1,7 +1,11 @@
 package DataTypesAndVariables_Ex;
 
+import java.util.Scanner;
+
 public class _10_PokeMon {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+
     }
 }
