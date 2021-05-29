9585fad57180a272e5cdee44276bc8b572e54cc3
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-29 17:01:42 +0300

    Update _10_PokeMon.java

--- Fundamentals/src/DataTypesAndVariables_Ex/_10_PokeMon.java
+++ Fundamentals/src/DataTypesAndVariables_Ex/_10_PokeMon.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class _10_PokeMon {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int pokePowerN = Integer.parseInt(scan.nextLine());
+        int startPower = pokePowerN;
+        int distanceM = Integer.parseInt(scan.nextLine());
+        int exhaustionFactorY = Integer.parseInt(scan.nextLine());
+        int count = 0;
 
+        while (pokePowerN >= distanceM) {
+            pokePowerN -= distanceM;
+            count++;
+            if (pokePowerN == startPower / 2) {
+                pokePowerN /= exhaustionFactorY;
+            }
+        }
+        System.out.println(pokePowerN);
+        System.out.println(count);
 
     }
 }
