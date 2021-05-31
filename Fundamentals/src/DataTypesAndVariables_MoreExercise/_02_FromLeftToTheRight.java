61247ad84392ff2d60ee10cb83594a5a11fd7e5c
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-31 21:53:43 +0300

    Create _02_FromLeftToTheRight.java

---
+++ Fundamentals/src/DataTypesAndVariables_MoreExercise/_02_FromLeftToTheRight.java
@@ -0,0 +1,10 @@
+package DataTypesAndVariables_MoreExercise;
+
+import java.util.Scanner;
+
+public class _02_FromLeftToTheRight {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = in
+    }
+}
