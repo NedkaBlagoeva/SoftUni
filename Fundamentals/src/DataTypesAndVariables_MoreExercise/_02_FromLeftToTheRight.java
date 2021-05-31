bf4fb5e0b64a52ac098792725f731ad4a2c6ffac
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-05-31 22:35:35 +0300

    Update _02_FromLeftToTheRight.java

--- Fundamentals/src/DataTypesAndVariables_MoreExercise/_02_FromLeftToTheRight.java
+++ Fundamentals/src/DataTypesAndVariables_MoreExercise/_02_FromLeftToTheRight.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class _02_FromLeftToTheRight {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = in
+        int n = Integer.parseInt(scan.nextLine());
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            String lineOfNumbers = scan.nextLine();
+            String[] parts = lineOfNumbers.split(" ");
+            int number1 = Integer.parseInt(parts[0]);
+            int number2 = Integer.parseInt(parts[1]);
+
+            if (number1 > number2) {
+                while (number1 != 0) {
+                    sum += number1 % 10;
+                    number1 /= 10;
+                }
+            } else {
+                while (number2 != 0) {
+                    sum += number2 % 10;
+                    number2 /= 10;
+                }
+            }
+            System.out.println(sum);
+            sum = 0;
+        }
     }
 }
