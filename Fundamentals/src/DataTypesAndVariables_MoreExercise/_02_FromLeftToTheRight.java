089c7609172b611f16aa17292b674832b4712e0b
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-01 21:39:44 +0300

    Update _02_FromLeftToTheRight.java

--- Fundamentals/src/DataTypesAndVariables_MoreExercise/_02_FromLeftToTheRight.java
+++ Fundamentals/src/DataTypesAndVariables_MoreExercise/_02_FromLeftToTheRight.java
@@ -11,17 +11,17 @@ public class _02_FromLeftToTheRight {
         for (int i = 1; i <= n; i++) {
             String lineOfNumbers = scan.nextLine();
             String[] parts = lineOfNumbers.split(" ");
-            int number1 = Integer.parseInt(parts[0]);
-            int number2 = Integer.parseInt(parts[1]);
+            long number1 = Long.parseLong(parts[0]);
+            long number2 = Long.parseLong(parts[1]);
 
             if (number1 > number2) {
                 while (number1 != 0) {
-                    sum += number1 % 10;
+                    sum += Math.abs(number1 % 10);
                     number1 /= 10;
                 }
             } else {
                 while (number2 != 0) {
-                    sum += number2 % 10;
+                    sum += Math.abs(number2 % 10);
                     number2 /= 10;
                 }
             }
