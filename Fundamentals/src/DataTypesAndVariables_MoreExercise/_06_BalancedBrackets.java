e7b0eb3a50e299dc43b54d068805fe8d51b5a7d8
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-02 21:39:19 +0300

    Create _06_BalancedBrackets.java

---
+++ Fundamentals/src/DataTypesAndVariables_MoreExercise/_06_BalancedBrackets.java
@@ -0,0 +1,20 @@
+package DataTypesAndVariables_MoreExercise;
+
+import java.util.Scanner;
+
+public class _06_BalancedBrackets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        for (int i = 1; i <= n; i++) {
+            String input = scan.nextLine();
+            for (int j = 0; j < input.length(); j++) {
+
+                if ((input.charAt(j)){
+
+                }
+                System.out.println(input.charAt(j));
+            }
+        }
+    }
+}
