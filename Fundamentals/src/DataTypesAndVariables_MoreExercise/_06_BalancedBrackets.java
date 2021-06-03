80066cf8a655c6049367ee781d0d56fac712bbcc
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-03 15:35:06 +0300

    Update _06_BalancedBrackets.java

--- Fundamentals/src/DataTypesAndVariables_MoreExercise/_06_BalancedBrackets.java
+++ Fundamentals/src/DataTypesAndVariables_MoreExercise/_06_BalancedBrackets.java
@@ -6,15 +6,35 @@ public class _06_BalancedBrackets {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        for (int i = 1; i <= n; i++) {
+        int openBracket = 0;
+        int closeBracket = 0;
+        boolean isBalanced = false;
+        for (int line = 1; line <= n; line++) {
             String input = scan.nextLine();
-            for (int j = 0; j < input.length(); j++) {
 
-                if ((input.charAt(j)){
-
+            if (input.charAt(0) == 40) {
+                openBracket++;
+                if (openBracket - closeBracket >= 2) {
+                    isBalanced = false;
+                    break;
                 }
-                System.out.println(input.charAt(j));
+            } else if (input.charAt(0) == 41) {
+                closeBracket++;
+                if (closeBracket > openBracket) {
+                    isBalanced = false;
+                    break;
+                }
             }
+        }
+        if (openBracket == closeBracket) {
+            isBalanced = true;
+        } else {
+            isBalanced = false;
+        }
+        if (isBalanced) {
+            System.out.println("BALANCED");
+        } else {
+            System.out.println("UNBALANCED");
         }
     }
 }
