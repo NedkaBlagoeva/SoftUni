3452e099e070d25a87c0df0ef4ab823bc612cd8b
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-04 17:51:33 +0300

    Create _04_ReverseAnArrayOfStrings.java

---
+++ Fundamentals/src/Arrays_Lab/_04_ReverseAnArrayOfStrings.java
@@ -0,0 +1,19 @@
+package Arrays_Lab;
+
+import java.util.Scanner;
+
+public class _04_ReverseAnArrayOfStrings {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String [] array = scan.nextLine().split(" ");
+        String temp = "";
+        for (int i = 0; i < array.length/2; i++) {
+            temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
