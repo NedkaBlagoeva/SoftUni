84ce20200cecad0a4a6f143b9a9a5f3b514250d4
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-05 11:22:01 +0300

    Create _02_CommonElements.java

---
+++ Fundamentals/src/Array_Exercise/_02_CommonElements.java
@@ -0,0 +1,10 @@
+package Array_Exercise;
+
+import java.util.Scanner;
+
+public class _02_CommonElements {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
