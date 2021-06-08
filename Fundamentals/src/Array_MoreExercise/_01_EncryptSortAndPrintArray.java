47c39efafda2f3972453528b5b6eb06d1d7115fa
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-08 17:03:53 +0300

    Create _01_EncryptSortAndPrintArray.java

---
+++ Fundamentals/src/Array_MoreExercise/_01_EncryptSortAndPrintArray.java
@@ -0,0 +1,10 @@
+package Array_MoreExercise;
+
+import java.util.Scanner;
+
+public class _01_EncryptSortAndPrintArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
