16262d732b5439b408a8918b8eebd0ee53672b39
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-10 15:15:52 +0300

    Create _07_RepeatString.java

---
+++ Fundamentals/src/Methods_Lab/_07_RepeatString.java
@@ -0,0 +1,9 @@
+package Methods_Lab;
+
+import java.util.Scanner;
+
+public class _07_RepeatString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
