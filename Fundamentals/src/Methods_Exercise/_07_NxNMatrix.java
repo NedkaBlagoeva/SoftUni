160b7d83f555100119e12dbe1c3361a200b0e22c
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-15 12:50:39 +0300

    Create _07_NxNMatrix.java

---
+++ Fundamentals/src/Methods_Exercise/_07_NxNMatrix.java
@@ -0,0 +1,24 @@
+package Methods_Exercise;
+
+import java.util.Scanner;
+
+public class _07_NxNMatrix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        printNxNMatrix(num);
+    }
+
+    private static void printNxNMatrix(int num) {
+        for (int row = 1; row <= num; row++) {
+            printSingleLine(num);
+            System.out.println();
+        }
+    }
+
+    private static void printSingleLine(int num) {
+        for (int i = 1; i <= num; i++) {
+            System.out.print(num + " ");
+        }
+    }
+}
