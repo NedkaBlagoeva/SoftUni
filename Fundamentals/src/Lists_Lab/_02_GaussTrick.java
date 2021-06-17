91fe21b30f507c4891f2f9456cbdd911c2c30c20
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-17 21:32:26 +0300

    Create _02_GaussTrick.java

---
+++ Fundamentals/src/Lists_Lab/_02_GaussTrick.java
@@ -0,0 +1,24 @@
+package Lists_Lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class _02_GaussTrick {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scan.nextLine()
+                .split(" ")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int halfSize = numbers.size() / 2;
+        for (int i = 0; i < halfSize; i++) {
+            int sum = numbers.get(i) + numbers.get(numbers.size() - 1);
+            numbers.set(i, sum);
+            numbers.remove(numbers.size() - 1);
+        }
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
