739e7662b8f8f60900685eda6e8f925e5e18c3be
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-18 14:37:52 +0300

    Create _06_ListOfProducts.java

---
+++ Fundamentals/src/Lists_Lab/_06_ListOfProducts.java
@@ -0,0 +1,20 @@
+package Lists_Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _06_ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        String[] products = new String[n];
+        for (int i = 0; i < products.length; i++) {
+            String product = scan.nextLine();
+            products[i] = product;
+        }
+        Arrays.sort(products);
+        for (int i = 0; i < products.length; i++) {
+            System.out.println(i + 1 + "." + products[i]);
+        }
+    }
+}
