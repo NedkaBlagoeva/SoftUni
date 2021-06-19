f10c55eb694deeed57a4f9654229187abf37e742
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-19 14:40:33 +0300

    Create _03_HouseParty.java

---
+++ Fundamentals/src/List_Exercise/_03_HouseParty.java
@@ -0,0 +1,15 @@
+package List_Exercise;
+
+import java.util.Scanner;
+
+public class _03_HouseParty {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberOfCommands = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < numberOfCommands; i++) {
+            String command = scan.nextLine();
+            String [] commandData = command.split("\\s+");
+
+        }
+    }
+}
