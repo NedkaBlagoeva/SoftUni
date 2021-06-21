18328a5044d14e5d89bcc9bcc4c248a575b7bf11
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-21 20:59:55 +0300

    Create _06_CardsGame.java

---
+++ Fundamentals/src/List_Exercise/_06_CardsGame.java
@@ -0,0 +1,11 @@
+package List_Exercise;
+
+import java.util.Scanner;
+
+public class _06_CardsGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+
+    }
+}
