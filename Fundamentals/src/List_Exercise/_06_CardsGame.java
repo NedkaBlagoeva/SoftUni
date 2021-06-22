ba59379b578ea5ce06dc6119b0080154f49834f9
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-22 15:04:09 +0300

    Update _06_CardsGame.java

--- Fundamentals/src/List_Exercise/_06_CardsGame.java
+++ Fundamentals/src/List_Exercise/_06_CardsGame.java
@@ -1,11 +1,12 @@
 package List_Exercise;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class _06_CardsGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        List <Integer> firstPlayerCards = Array
 
     }
 }
