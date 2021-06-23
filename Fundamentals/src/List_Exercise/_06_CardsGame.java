1ba10ada7891428c0aaab080c3215911da28f5f5
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-23 19:27:26 +0300

    Update _06_CardsGame.java

--- Fundamentals/src/List_Exercise/_06_CardsGame.java
+++ Fundamentals/src/List_Exercise/_06_CardsGame.java
@@ -12,35 +12,34 @@ public class _06_CardsGame {
                 .map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> secondPlayerCards = Arrays.stream(scan.nextLine().split(" "))
                 .map(Integer::parseInt).collect(Collectors.toList());
-//        // int index = 0;
-//        while (!firstPlayerCards.isEmpty() || !secondPlayerCards.isEmpty()) {
-//            if (firstPlayerCards.get(0) > secondPlayerCards.get(0)) {
-//            }
-//                firstPlayerCards.add(secondPlayerCards.get(0));
-//                firstPlayerCards.add(firstPlayerCards.get(0));
-//                firstPlayerCards.remove(0);
-//                secondPlayerCards.remove(0);
-//            } else if (secondPlayerCards.get(0) > firstPlayerCards.get(0)) {
-//                secondPlayerCards.add(firstPlayerCards.get(0));
-//                secondPlayerCards.add(secondPlayerCards.get(0));
-//                firstPlayerCards.remove(0);
-//                secondPlayerCards.remove(0);
-//            } else {
-//                firstPlayerCards.remove(0);
-//                secondPlayerCards.remove(0);
-           // }
+        while (!firstPlayerCards.isEmpty() && !secondPlayerCards.isEmpty()) {
+            if (firstPlayerCards.get(0) > secondPlayerCards.get(0)) {
+                firstPlayerCards.add(secondPlayerCards.get(0));
+                firstPlayerCards.add(firstPlayerCards.get(0));
+                firstPlayerCards.remove(0);
+                secondPlayerCards.remove(0);
+            } else if (secondPlayerCards.get(0) > firstPlayerCards.get(0)) {
+                secondPlayerCards.add(firstPlayerCards.get(0));
+                secondPlayerCards.add(secondPlayerCards.get(0));
+                firstPlayerCards.remove(0);
+                secondPlayerCards.remove(0);
+            } else {
+                firstPlayerCards.remove(0);
+                secondPlayerCards.remove(0);
+            }
 
-//        int sum = 0;
-//        if (firstPlayerCards.isEmpty()) {
-//            for (Integer Card : secondPlayerCards) {
-//                sum += Card;
-//            }
-//            System.out.printf("Second player wins! Sum: %d", sum);
-//        } else if (secondPlayerCards.isEmpty()) {
-//            for (Integer Card : firstPlayerCards) {
-//                sum += Card;
-//            }
-//            System.out.printf("First player wins! Sum: %d", sum);
-//        }
+            int sum = 0;
+            if (firstPlayerCards.isEmpty()) {
+                for (Integer Card : secondPlayerCards) {
+                    sum += Card;
+                }
+                System.out.printf("Second player wins! Sum: %d", sum);
+            } else if (secondPlayerCards.isEmpty()) {
+                for (Integer Card : firstPlayerCards) {
+                    sum += Card;
+                }
+                System.out.printf("First player wins! Sum: %d", sum);
+            }
+        }
     }
 }
