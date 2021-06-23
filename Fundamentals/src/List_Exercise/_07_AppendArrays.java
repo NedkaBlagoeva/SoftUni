7d478a1a9e96caed64f2869b6345ba50b5964a81
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-23 19:27:21 +0300

    Create _07_AppendArrays.java

---
+++ Fundamentals/src/List_Exercise/_07_AppendArrays.java
@@ -0,0 +1,24 @@
+package List_Exercise;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class _07_AppendArrays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // 1 2 3 |4 5 6 |  7  8
+        List<String> numbersArr = Arrays.stream(scan.nextLine().split("\\|")).collect(Collectors.toList());
+        Collections.reverse(numbersArr);
+        String output = numbersArr.toString()
+                .replace("[", "")
+                .replace("]", "")
+                .replaceAll(",", "")
+                .trim()
+                .replaceAll("\\s+", " ");
+        System.out.println(output);
+    }
+}
