ea70f585c94c24d5a7a78dac741c4e80e128052e
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-28 19:45:51 +0300

    Create Student.java

---
+++ Fundamentals/src/ObjectsAndClasses_Exercise/Students/Student.java
@@ -0,0 +1,22 @@
+package ObjectsAndClasses_Exercise.Students;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private double grade;
+
+    public Student(String firstName, String lastName, double grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s: %.2f", this.firstName, this.lastName, this.grade);
+    }
+}
