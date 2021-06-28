23ea3b2ca6b3256e0f65ed9b7c94ff77cf465e96
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-28 18:26:58 +0300

    Create Main.java

---
+++ Fundamentals/src/ObjectsAndClasses_Exercise/Articles2/Main.java
@@ -0,0 +1,9 @@
+package ObjectsAndClasses_Exercise.Articles2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
