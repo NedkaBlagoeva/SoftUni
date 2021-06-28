fc701d3e55d90093872a2ba34a3f388193dc4b6a
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-28 18:27:08 +0300

    Create Person.java

---
+++ Fundamentals/src/ObjectsAndClasses_Exercise/OpinionPoll/Person.java
@@ -0,0 +1,24 @@
+package ObjectsAndClasses_Exercise.OpinionPoll;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age;
+    }
+}
