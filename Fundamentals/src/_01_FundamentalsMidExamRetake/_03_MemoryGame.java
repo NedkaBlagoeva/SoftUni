822ec1feab208fb15a5eb4f04a1d2c660ba16697
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-06-30 21:39:09 +0300

    Update _03_MemoryGame.java

--- Fundamentals/src/_01_FundamentalsMidExamRetake/_03_MemoryGame.java
+++ Fundamentals/src/_01_FundamentalsMidExamRetake/_03_MemoryGame.java
@@ -8,52 +8,59 @@ import java.util.stream.Collectors;
 public class _03_MemoryGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List<String> inputSequence = Arrays
+        List<String> sequence = Arrays
                 .stream(scan.nextLine().split("\\s+"))
                 .collect(Collectors.toList());
         int count = 0;
         String input = scan.nextLine();
         while (!"end".equals(input)) {
-            if(inputSequence.isEmpty()){
+            if (sequence.isEmpty()) {
                 break;
             }
             count++;
             int index1 = Integer.parseInt(input.split("\\s+")[0]);
             int index2 = Integer.parseInt(input.split("\\s+")[1]);
-            if (!isValidIndex(inputSequence.size(), index1) ||
-                    !isValidIndex(inputSequence.size(), index2) ||
-                    index1 == index2) {
+            if (isCheating(sequence, index1, index2)) {
                 System.out.println("Invalid input! Adding additional elements to the board");
-                inputSequence.add(inputSequence.size() / 2, "-" + count + "a");
-                inputSequence.add(inputSequence.size() / 2, "-" + count + "a");
+                sequence.add(sequence.size() / 2, "-" + count + "a");
+                sequence.add(sequence.size() / 2, "-" + count + "a");
                 input = scan.nextLine();
                 continue;
             }
-            if (inputSequence.get(index1).equals(inputSequence.get(index2))) {
-                System.out.printf("Congrats! You have found matching elements - %s!%n", inputSequence.get(index1));
+            if (sequence.get(index1).equals(sequence.get(index2))) {
+                System.out.printf("Congrats! You have found matching elements - %s!%n", sequence.get(index1));
                 if (index1 > index2) {
-                    inputSequence.remove(index1);
-                    inputSequence.remove(index2);
+                    sequence.remove(index1);
+                    sequence.remove(index2);
                 } else {
-                    inputSequence.remove(index2);
-                    inputSequence.remove(index1);
+                    sequence.remove(index2);
+                    sequence.remove(index1);
                 }
             } else {
                 System.out.println("Try again!");
             }
             input = scan.nextLine();
         }
-        if (inputSequence.isEmpty()) {
+        if (sequence.isEmpty()) {
             System.out.printf("You have won in %d turns!", count);
         } else {
             System.out.println("Sorry you lose :(");
-            for (String s : inputSequence) {
+            for (String s : sequence) {
                 System.out.print(s + " ");
             }
         }
     }
 
-    public static boolean isValidIndex(int length, int index) {
-        return index >= 0 && index < length;
+    private static boolean isCheating(List<String> sequence, int index1, int index2) {
+        if (index1 == index2) {
+            return true;
+        }
+        if (index1 < 0 || index1 >= sequence.size()) {
+            return true;
+        }
+        if (index2 < 0 || index2 >= sequence.size()) {
+            return true;
+        }
+        return false;
     }
 }
