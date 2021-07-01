957406d13fdf42a97cea0c7d0b05e8e53888db9d
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-01 21:07:20 +0300

    Create _02_ArrayModifier.java

---
+++ Fundamentals/src/_02_FundamentalsMidExam/_02_ArrayModifier.java
@@ -0,0 +1,10 @@
+package _02_FundamentalsMidExam;
+
+import java.util.Scanner;
+
+public class _02_ArrayModifier {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
