de04f444a70d480aa94ee0bcc821aa53a62462be
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-03 00:18:22 +0300

    Create _01_CounterStrike.java

---
+++ Fundamentals/src/_03_FundamentalsMidExamRetake/_01_CounterStrike.java
@@ -0,0 +1,31 @@
+package _03_FundamentalsMidExamRetake;
+
+import java.util.Scanner;
+
+public class _01_CounterStrike {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int energy = Integer.parseInt(scan.nextLine());
+        String input = scan.nextLine();
+        int wins = 0;
+        while (!"End of battle".equals(input)){
+//            if (energy == 0){
+//                break;
+//            }
+            int distance = Integer.parseInt(input);
+            if (energy >= distance){
+                energy -= distance;
+                wins++;
+                if (wins % 3 == 0){
+                    energy +=wins;
+                }
+            } else {
+                System.out.printf("Not enough energy! Game ends with %d" +
+                        " won battles and %d energy", wins, energy);
+                return;
+            }
+            input = scan.nextLine();
+        }
+        System.out.printf("Won battles: %d. Energy left: %d", wins, energy);
+    }
+}
