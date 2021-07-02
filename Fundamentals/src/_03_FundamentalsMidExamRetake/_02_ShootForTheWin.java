2d72eef08506bfcdc25e918b1d59284ce5670d22
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-03 00:18:26 +0300

    Create _02_ShootForTheWin.java

---
+++ Fundamentals/src/_03_FundamentalsMidExamRetake/_02_ShootForTheWin.java
@@ -0,0 +1,10 @@
+package _03_FundamentalsMidExamRetake;
+
+import java.util.Scanner;
+
+public class _02_ShootForTheWin {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
