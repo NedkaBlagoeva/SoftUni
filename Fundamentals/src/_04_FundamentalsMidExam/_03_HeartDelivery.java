361d51b3939a58c1fb0210265a3aa0740cdd997c
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-04 13:18:05 +0300

    Create _03_HeartDelivery.java

---
+++ Fundamentals/src/_04_FundamentalsMidExam/_03_HeartDelivery.java
@@ -0,0 +1,10 @@
+package _04_FundamentalsMidExam;
+
+import java.util.Scanner;
+
+public class _03_HeartDelivery {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
