9a5b85f0ca278b58be32c576467cba34f7bb1241
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-07 17:58:51 +0300

    Create _03_Inventory.java

---
+++ Fundamentals/src/_05_FundamentalsMidExam/_03_Inventory.java
@@ -0,0 +1,10 @@
+package _05_FundamentalsMidExam;
+
+import java.util.Scanner;
+
+public class _03_Inventory {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
