757b04ea0db348618f3350619286892213676307
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-09 00:32:13 +0300

    Create _01_BlackFlag.java

---
+++ Fundamentals/src/_06_FundamentalsMidExamRetake6August2019/_01_BlackFlag.java
@@ -0,0 +1,26 @@
+package _06_FundamentalsMidExamRetake6August2019;
+
+import java.util.Scanner;
+
+public class _01_BlackFlag {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int days = Integer.parseInt(scan.nextLine());
+        int plunderForDay = Integer.parseInt(scan.nextLine());
+        double expectedPlunder = Double.parseDouble(scan.nextLine());
+        double collectedPlunder = 0;
+        for (int i = 1; i <= days; i++) {
+            collectedPlunder += plunderForDay;
+            if (i % 3 == 0) {
+                collectedPlunder += plunderForDay * 1.0 / 2;
+            } else if (i % 5 == 0) {
+                collectedPlunder *= 0.7;
+            }
+        }
+        if (collectedPlunder >= expectedPlunder) {
+            System.out.printf("Ahoy! %.2f plunder gained.", collectedPlunder);
+        } else {
+            System.out.printf("Collected only %.2f%% of the plunder.", collectedPlunder / expectedPlunder * 100);
+        }
+    }
+}
