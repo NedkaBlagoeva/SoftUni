9398511de89cd1de840e06f9928d289909f679bc
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-16 00:01:08 +0300

    Update _01_CountRealNumbers.java

--- Fundamentals/src/_08_MapsLambdaAndStreamAPI/Lab/_01_CountRealNumbers.java
+++ Fundamentals/src/_08_MapsLambdaAndStreamAPI/Lab/_01_CountRealNumbers.java
@@ -1,4 +1,23 @@
 package _08_MapsLambdaAndStreamAPI.Lab;
 
+import java.util.*;
+
 public class _01_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double[] numbers = Arrays.stream(scan.nextLine()
+                .split(" "))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+        Map<Double, Integer> counts = new TreeMap<>();
+        for (double number : numbers) {
+            if (!counts.containsKey(number)) {
+                counts.put(number, 0);
+            }
+            counts.put(number, counts.get(number) + 1);
+        }
+        for (Map.Entry<Double, Integer> entry : counts.entrySet()) {
+            System.out.printf("%.0f -> %d%n", entry.getKey(), entry.getValue());
+        }
+    }
 }
