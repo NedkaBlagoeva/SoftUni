ffe9da0fa9b3b47d7e0fb253ec0d842afac4f837
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-17 23:20:54 +0300

    Update _03_OddOccurrences.java

--- Fundamentals/src/_08_MapsLambdaAndStreamAPI/Lab/_03_OddOccurrences.java
+++ Fundamentals/src/_08_MapsLambdaAndStreamAPI/Lab/_03_OddOccurrences.java
@@ -5,21 +5,21 @@ import java.util.*;
 public class _03_OddOccurrences {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String [] words = scan.nextLine().split(" ");
+        String[] words = scan.nextLine().split(" ");
         Map<String, Integer> wordCounts = new LinkedHashMap<>();
         for (String word : words) {
             String currentWord = word.toLowerCase();
             wordCounts.putIfAbsent(currentWord, 0);
             wordCounts.put(currentWord, wordCounts.get(currentWord) + 1);
         }
-        List <String> oddKeys = new ArrayList<>();
+        List<String> oddKeys = new ArrayList<>();
 
         for (var s : wordCounts.entrySet()) {
-            if (s.getValue() % 2 != 0){
+            if (s.getValue() % 2 != 0) {
                 oddKeys.add(s.getKey());
             }
         }
-        System.out.print(String.join(", ",oddKeys));
+        System.out.print(String.join(", ", oddKeys));
 
     }
 }
