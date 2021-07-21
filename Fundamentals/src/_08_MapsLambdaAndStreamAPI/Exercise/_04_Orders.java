6a22c2f2551f17ded23df55a8530542a811a8aac
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-21 17:46:50 +0300

    Update _04_Orders.java

--- Fundamentals/src/_08_MapsLambdaAndStreamAPI/Exercise/_04_Orders.java
+++ Fundamentals/src/_08_MapsLambdaAndStreamAPI/Exercise/_04_Orders.java
@@ -1,23 +1,28 @@
 package _08_MapsLambdaAndStreamAPI.Exercise;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class _04_Orders {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Map<String, Map<Double, Integer>> products = new LinkedHashMap<>();
+        Map<String, List<Double>> products = new LinkedHashMap<>();
         String input = scan.nextLine();
-        while (!"buy".equals(input)){
+        while (!"buy".equals(input)) {
             String product = input.split(" ")[0];
             double price = Double.parseDouble(input.split(" ")[1]);
-            int quantity = Integer.parseInt(input.split(" ")[2]);
-
-
-
-
-            input = scan.nextLine();
+            double quantity = Double.parseDouble(input.split(" ")[2]);
+            if (!products.containsKey(product)){
+                products.put(product, new ArrayList<>());
+                products.get(product).add(price);
+                products.get(product).add(quantity);
+            } else {
+                products.get(product).set(0,price);
+                products.get(product).set(1, (products.get(product).get(1) + quantity));
+            }
+                input = scan.nextLine();
+        }
+        for (Map.Entry <String, List<Double>> entry : products.entrySet()) {
+            System.out.printf("%s -> %.2f%n", entry.getKey(), entry.getValue().get(0) * entry.getValue().get(1));
         }
     }
 }
