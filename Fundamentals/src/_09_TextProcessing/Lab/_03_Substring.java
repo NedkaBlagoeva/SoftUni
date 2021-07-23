301e409b929a68f2c872c732aa9abb84a5c13ff4
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-23 22:19:21 +0300

    Create _03_Substring.java

---
+++ Fundamentals/src/_09_TextProcessing/Lab/_03_Substring.java
@@ -0,0 +1,16 @@
+package _09_TextProcessing.Lab;
+
+import java.util.Scanner;
+
+public class _03_Substring {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String textToRemove = scan.nextLine();
+        String text = scan.nextLine();
+        while (text.contains(textToRemove)){
+            text = text.replace(textToRemove, "");
+        }
+
+        System.out.println(text);
+    }
+}
