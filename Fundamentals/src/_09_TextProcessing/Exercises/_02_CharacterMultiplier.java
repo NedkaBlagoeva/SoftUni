b8dd5512a40b78aac38f47c7f8fec77f5999adef
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-24 01:11:34 +0300

    Create _02_CharacterMultiplier.java

---
+++ Fundamentals/src/_09_TextProcessing/Exercises/_02_CharacterMultiplier.java
@@ -0,0 +1,10 @@
+package _09_TextProcessing.Exercises;
+
+import java.util.Scanner;
+
+public class _02_CharacterMultiplier {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
