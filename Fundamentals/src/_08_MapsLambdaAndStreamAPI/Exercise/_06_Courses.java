fb107747263dcbbc2024454b44b6cf008a09483c
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-23 16:14:07 +0300

    Create _06_Courses.java

---
+++ Fundamentals/src/_08_MapsLambdaAndStreamAPI/Exercise/_06_Courses.java
@@ -0,0 +1,10 @@
+package _08_MapsLambdaAndStreamAPI.Exercise;
+
+import java.util.Scanner;
+
+public class _06_Courses {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
