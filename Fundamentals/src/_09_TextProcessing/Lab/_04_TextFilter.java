6ceb9cb24a458913ce7720d2e2d27c7e073e7434
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-23 22:20:23 +0300

    Update _04_TextFilter.java

--- Fundamentals/src/_09_TextProcessing/Lab/_04_TextFilter.java
+++ Fundamentals/src/_09_TextProcessing/Lab/_04_TextFilter.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class _04_TextFilter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String [] banWords = scan.nextLine().split(", ");
+        String[] banWords = scan.nextLine().split(", ");
         String text = scan.nextLine();
         for (String banWord : banWords) {
             text = text.replace(banWord, "*".repeat(banWord.length()));
