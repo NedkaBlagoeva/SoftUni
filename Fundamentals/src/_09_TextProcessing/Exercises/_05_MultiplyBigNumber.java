a6a88c0c866d2ad47516ad0652de52cf4d79b4a0
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-25 21:12:19 +0300

    Update _05_MultiplyBigNumber.java

--- Fundamentals/src/_09_TextProcessing/Exercises/_05_MultiplyBigNumber.java
+++ Fundamentals/src/_09_TextProcessing/Exercises/_05_MultiplyBigNumber.java
@@ -6,17 +6,30 @@ import java.util.Scanner;
 public class _05_MultiplyBigNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int [] bigNumber = Arrays.stream(scan.nextLine().split("")).mapToInt(Integer::parseInt).toArray();
+        int[] bigNumber = Arrays.stream(scan.nextLine().split("")).mapToInt(Integer::parseInt).toArray();
         int num = Integer.parseInt(scan.nextLine());
         StringBuilder result = new StringBuilder();
         int remainder = 0;
         int tens = 0;
+        if (bigNumber.length == 0){
+            System.out.println("0");
+            return;
+        }
         for (int i = bigNumber.length - 1; i >= 0; i--) {
-            int num1 = Integer.parseInt(String.valueOf(bigNumber[i]));
+            int num1 = bigNumber[i];
             int midResult = num1 * num;
-            if (midResult > 9){
-                result.append(midResult % 10);
+            remainder = midResult % 10;
+            result.append(remainder + tens);
+            if (midResult > 9) {
+                tens = midResult / 10;
+            } else {
+                tens = 0;
             }
         }
+        if (tens != 0){
+            result.append(tens);
+        }
+
+        System.out.println(result.reverse());
     }
 }
