6c4dfcbf86b77570bd8a3d675018e923da3705e0
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-07-31 13:47:32 +0300

    Create _02_MatchPhoneNumber.java

---
+++ Fundamentals/src/_10_RegularExpressions/Lab/_02_MatchPhoneNumber.java
@@ -0,0 +1,23 @@
+package _10_RegularExpressions.Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class _02_MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        List <String> matchedPhones = new ArrayList<>();
+        
+        while (matcher.find()){
+            matchedPhones.add(matcher.group());
+        }
+        System.out.print(String.join(", ", matchedPhones));
+    }
+}
