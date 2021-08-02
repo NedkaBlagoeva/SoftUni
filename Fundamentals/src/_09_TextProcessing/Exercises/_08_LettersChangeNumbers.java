ef2d9a55fb216c31652c66d9fa40a93865718d47
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-02 22:35:54 +0300

    Update _08_LettersChangeNumbers.java

--- Fundamentals/src/_09_TextProcessing/Exercises/_08_LettersChangeNumbers.java
+++ Fundamentals/src/_09_TextProcessing/Exercises/_08_LettersChangeNumbers.java
@@ -7,14 +7,25 @@ public class _08_LettersChangeNumbers {
         Scanner scan = new Scanner(System.in);
         String[] words = scan.nextLine().split("\\s+");
         double sum = 0;
+        double number = 0;
         for (String word : words) {
             char firstSymbol = word.charAt(0);
             char lastSymbol = word.charAt(word.length() - 1);
-            int number = Integer.parseInt(word.substring(1, word.length() - 1));
-            if (firstSymbol > 'A' && firstSymbol < 'Z'){
-                
+            number = Integer.parseInt(word.substring(1, word.length() - 1));
+            if (firstSymbol >= 'A' && firstSymbol <= 'Z') {
+                number /= (firstSymbol - 64);
             }
-            System.out.println();
+            if (firstSymbol >= 'a' && firstSymbol <= 'z') {
+                number *= (firstSymbol - 96);
+            }
+            if (lastSymbol >= 'A' && lastSymbol <= 'Z') {
+                number -= (lastSymbol - 64);
+            }
+            if (lastSymbol >= 'a' && lastSymbol <= 'z') {
+                number += (lastSymbol - 96);
+            }
+            sum += number;
         }
+        System.out.printf("%.2f", sum);
     }
 }
