857f643d80e1527208c306cfecc062dc638f1383
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-02 22:36:02 +0300

    Create _02_AsciiSumator.java

---
+++ Fundamentals/src/_09_TextProcessing/MoreExercise/_02_AsciiSumator.java
@@ -0,0 +1,19 @@
+package _09_TextProcessing.MoreExercise;
+
+import java.util.Scanner;
+
+public class _02_AsciiSumator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char startChar = scan.nextLine().charAt(0);
+        char endChar = scan.nextLine().charAt(0);
+        String text = scan.nextLine();
+        int sum = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) >= startChar && text.charAt(i) <= endChar) {
+                sum += text.charAt(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
