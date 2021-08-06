392581a7e51decb3308a18a36e039bc4a35ffb9d
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-06 15:19:15 +0300

    Create _03_HeroesOfCodeAndLogicVII.java

---
+++ Fundamentals/src/FinalExam/_04_FinalExam/_03_HeroesOfCodeAndLogicVII.java
@@ -0,0 +1,10 @@
+package FinalExam._04_FinalExam;
+
+import java.util.Scanner;
+
+public class _03_HeroesOfCodeAndLogicVII {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
