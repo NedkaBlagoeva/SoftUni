33ec986fe5e6b0a3de70bed5da5e205431e1bae6
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-09 18:40:38 +0300

    Create _03_ThePianist.java

---
+++ Fundamentals/src/FinalExam/_01_FinalExamRetake/_03_ThePianist.java
@@ -0,0 +1,9 @@
+package FinalExam._01_FinalExamRetake;
+
+import java.util.Scanner;
+
+public class _03_ThePianist {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
