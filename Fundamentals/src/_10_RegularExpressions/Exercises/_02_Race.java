b49eec0e4dcff1ab71c5b0de655540a68a4a55a5
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-11 19:05:16 +0300

    Create _02_Race.java

---
+++ Fundamentals/src/_10_RegularExpressions/Exercises/_02_Race.java
@@ -0,0 +1,10 @@
+package _10_RegularExpressions.Exercises;
+
+import java.util.Scanner;
+
+public class _02_Race {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
