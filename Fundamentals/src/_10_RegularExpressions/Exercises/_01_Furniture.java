98df75daab05a6dfa38594154360f870ad89ce32
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-11 18:16:38 +0300

    Create _01_Furniture.java

---
+++ Fundamentals/src/_10_RegularExpressions/Exercises/_01_Furniture.java
@@ -0,0 +1,14 @@
+package _10_RegularExpressions.Exercises;
+
+import java.util.Scanner;
+
+public class _01_Furniture {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        while (!"Purchase".equals(input)){
+
+            input = scan.nextLine();
+        }
+    }
+}
