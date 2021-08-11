d45b0b1552ae12eb7657c50fe3f8ada05916a2cb
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-11 18:45:01 +0300

    Update _01_Furniture.java

--- Fundamentals/src/_10_RegularExpressions/Exercises/_01_Furniture.java
+++ Fundamentals/src/_10_RegularExpressions/Exercises/_01_Furniture.java
@@ -1,14 +1,27 @@
 package _10_RegularExpressions.Exercises;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class _01_Furniture {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        while (!"Purchase".equals(input)){
-
+        String regex = ">>(?<furniture>[A-Za-z]+)<<(?<price>\\d+.?\\d*)!(?<quantity>\\d+)";
+        Pattern pattern = Pattern.compile(regex);
+        double totalMoney = 0;
+        System.out.println("Bought furniture:");
+        while (!"Purchase".equals(input)) {
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                System.out.println(matcher.group("furniture"));
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+                totalMoney += price * quantity;
+            }
             input = scan.nextLine();
         }
+        System.out.printf("Total money spend: %.2f", totalMoney);
     }
 }
