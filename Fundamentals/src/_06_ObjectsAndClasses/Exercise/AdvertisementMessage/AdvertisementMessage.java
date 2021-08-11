4a1736f9252aa4511415f09e167523bdae7af2e7
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-11 18:16:34 +0300

    Update AdvertisementMessage.java

--- Fundamentals/src/_06_ObjectsAndClasses/Exercise/AdvertisementMessage/AdvertisementMessage.java
+++ Fundamentals/src/_06_ObjectsAndClasses/Exercise/AdvertisementMessage/AdvertisementMessage.java
@@ -2,20 +2,20 @@ package _06_ObjectsAndClasses.Exercise.AdvertisementMessage;
 
 public class AdvertisementMessage {
     //laudatory phrase + event + author + city
-    private String Phase;
+    private String phase;
     private String event;
     private String author;
     private String city;
 
-    public AdvertisementMessage (String Phase, String event, String author, String city){
-        this.Phase = Phase;
+    public AdvertisementMessage (String phase, String event, String author, String city){
+        this.phase = phase;
         this.event = event;
         this.author = author;
         this.city = city;
     }
 
     public String toString (){
-        return this.Phase + " " + this.event + " " + this.author + " - " + this.city;
+        return this.phase + " " + this.event + " " + this.author + " - " + this.city;
         //{phrase} {event} {author} – {city}
     }
 }
