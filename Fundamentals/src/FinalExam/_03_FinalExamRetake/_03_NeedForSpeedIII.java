2b80b3b02ca2fa26fd848e6787248493367e3856
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-13 13:53:22 +0300

    Create _03_NeedForSpeedIII.java

---
+++ Fundamentals/src/FinalExam/_03_FinalExamRetake/_03_NeedForSpeedIII.java
@@ -0,0 +1,9 @@
+package FinalExam._03_FinalExamRetake;
+
+import java.util.Scanner;
+
+public class _03_NeedForSpeedIII {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
