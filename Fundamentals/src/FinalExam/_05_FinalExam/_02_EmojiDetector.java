dc8ae28b34c949841bcc18010c24923cf9fc046e
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-13 17:23:53 +0300

    Update _02_EmojiDetector.java

--- Fundamentals/src/FinalExam/_05_FinalExam/_02_EmojiDetector.java
+++ Fundamentals/src/FinalExam/_05_FinalExam/_02_EmojiDetector.java
@@ -1,4 +1,10 @@
 package FinalExam._05_FinalExam;
 
+import java.util.Scanner;
+
 public class _02_EmojiDetector {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
 }
