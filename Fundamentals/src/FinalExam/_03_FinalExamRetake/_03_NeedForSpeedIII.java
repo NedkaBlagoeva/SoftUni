407c4be30432ed019ca66bedeb3c12ef070fa4ec
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-13 16:04:58 +0300

    Update _03_NeedForSpeedIII.java

--- Fundamentals/src/FinalExam/_03_FinalExamRetake/_03_NeedForSpeedIII.java
+++ Fundamentals/src/FinalExam/_03_FinalExamRetake/_03_NeedForSpeedIII.java
@@ -24,29 +24,53 @@ public class _03_NeedForSpeedIII {
                     int distance = Integer.parseInt(commands[2]);
                     int fuelNeeded = Integer.parseInt(commands[3]);
                     int currentFuel = cars.get(carName).getFuel();
-                    if (currentFuel < fuelNeeded){
+                    if (currentFuel < fuelNeeded) {
                         System.out.println("Not enough fuel to make that ride");
                     } else {
                         int currentMileage = cars.get(carName).getMileage();
                         cars.get(carName).setMileage(currentMileage + distance);
-                        cars.get(carName).setFuel(currentFuel - distance);
+                        cars.get(carName).setFuel(currentFuel - fuelNeeded);
+                        System.out.printf("%s driven for %d kilometers. %d liters of fuel consumed.%n", carName, distance, fuelNeeded);
                     }
-                    if (cars.get(carName).getMileage() >= 100000){
+                    if (cars.get(carName).getMileage() >= 100000) {
                         cars.remove(carName);
-                        System.out.println("Time to sell the car " + carName);
+                        System.out.println("Time to sell the " + carName + "!");
                     }
                     break;
                 case "Refuel":
                     int fuelToRefuel = Integer.parseInt(commands[2]);
-
+                    int fuelToFill = 75 - cars.get(carName).getFuel(); // 75 - currentFuel
+                    if (fuelToFill < fuelToRefuel) {
+                        cars.get(carName).setFuel(75);
+                        System.out.printf("%s refueled with %d liters%n", carName, fuelToFill);
+                    } else {
+                        cars.get(carName).setFuel(cars.get(carName).getFuel() + fuelToRefuel);
+                        System.out.printf("%s refueled with %d liters%n", carName, fuelToRefuel);
+                    }
                     break;
                 case "Revert":
+                    int kilometers = Integer.parseInt(commands[2]);
+                    int currentMileage = cars.get(carName).getMileage();
+                    cars.get(carName).setMileage(currentMileage - kilometers);
+                    if (cars.get(carName).getMileage() < 10000) {
+                        cars.get(carName).setMileage(10000);
+                    } else {
+                        System.out.printf("%s mileage decreased by %d kilometers%n", carName, kilometers);
+                    }
                     break;
                 default:
                     break;
             }
             inputForCommands = scan.nextLine();
         }
+        cars.entrySet().stream().sorted((m1, m2) -> {
+            int result = Integer.compare(m2.getValue().getMileage(), m1.getValue().getMileage());
+            if (result == 0) {
+                result = m1.getKey().compareTo(m2.getKey());
+            }
+            return result;
+        }).forEach(car -> System.out.printf("%s -> Mileage: %d kms, Fuel in the tank: %d lt.%n",
+                car.getKey(), car.getValue().getMileage(), car.getValue().getFuel()));
     }
 
     public static class CarInfo {
