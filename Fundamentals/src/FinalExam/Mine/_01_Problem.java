fa6318eba2ca560826d848cecb36f08163ceb6fc
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-08-14 11:38:22 +0300

    Create _01_Problem.java

---
+++ Fundamentals/src/FinalExam/Mine/_01_Problem.java
@@ -0,0 +1,53 @@
+package FinalExam.Mine;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class _01_Problem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String email = scan.nextLine();
+        String commands = scan.nextLine();
+        while (!"Complete".equals(commands)) {
+            String[] commandsParts = commands.split("\\s+");
+            String command = commandsParts[0];
+            switch (command) {
+                case "Make":
+                    String secondWord = commandsParts[1];
+                    if (secondWord.equals("Upper")) {
+                        email = email.toUpperCase();
+                        System.out.println(email);
+                    } else if (secondWord.equals("Lower")) {
+                        email = email.toLowerCase();
+                        System.out.println(email);
+                    }
+                    break;
+                case "GetDomain":
+                    int count = Integer.parseInt(commandsParts[1]);
+                    System.out.println(email.substring(email.length() - count));
+                    break;
+                case "GetUsername":
+                    if (email.contains("@")) {
+                        System.out.println(email.substring(0, email.indexOf("@")));
+                    } else {
+                        System.out.printf("The email %s doesn't contain the @ symbol.%n", email);
+                    }
+                    break;
+                case "Replace":
+                    String symbol = commandsParts[1];
+                    email = email.replace(symbol, "-");
+                    System.out.println(email);
+                    break;
+                case "Encrypt":
+                    char[] emailSymbols = email.toCharArray();
+                    for (char emailSymbol : emailSymbols) {
+                        System.out.print((int) emailSymbol + " ");
+                    }
+                    break;
+                default:
+                    break;
+            }
+            commands = scan.nextLine();
+        }
+    }
+}
