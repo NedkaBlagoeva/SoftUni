520024e25e9ff68a9ee591e1d999ad192cbce957
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 16:37:21 +0300

    Update _03_DecimalToBinary.java

--- JavaAdvanced/src/StacksAndQueues/Lab/_03_DecimalToBinary.java
+++ JavaAdvanced/src/StacksAndQueues/Lab/_03_DecimalToBinary.java
@@ -1,10 +1,23 @@
 package StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class _03_DecimalToBinary {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        int input = Integer.parseInt(scan.nextLine());
+        if (input == 0) {
+            System.out.println(0);
+            return;
+        }
+        while (input > 0) {
+            result.push(input % 2);
+            input /= 2;
+        }
+        while (!result.isEmpty()) {
+            System.out.print(result.pop());
+        }
     }
 }
