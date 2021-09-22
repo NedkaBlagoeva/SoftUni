8aef07df0b637bec26558bfe3741001c3086288f
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 16:37:26 +0300

    Create _03_MatchingBrackets.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_03_MatchingBrackets.java
@@ -0,0 +1,19 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _03_MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque<Integer> bracketsIndexes = new ArrayDeque<>();
+        String input = scan.nextLine();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                bracketsIndexes.push(i);
+            } else if (input.charAt(i) == ')') {
+                System.out.println(input.substring(bracketsIndexes.pop(), i + 1));
+            }
+        }
+    }
+}
