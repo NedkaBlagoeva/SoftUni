141bb84326b9a197fc94ad829e275f2ec1fa6b25
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 15:00:11 +0300

    Update _02_SimpleCalculator.java

--- JavaAdvanced/src/StacksAndQueues/Lab/_02_SimpleCalculator.java
+++ JavaAdvanced/src/StacksAndQueues/Lab/_02_SimpleCalculator.java
@@ -6,10 +6,23 @@ import java.util.Scanner;
 public class _02_SimpleCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayDeque <String> elements = new ArrayDeque<>();
-        String [] input = scan.nextLine().split("\\s+");
-        for (int i = input.length - 1; i >= 0 ; i--) {
-
+        ArrayDeque<String> elements = new ArrayDeque<>();
+        String[] input = scan.nextLine().split("\\s+");
+        for (int i = input.length - 1; i >= 0; i--) {
+            elements.push(input[i]);
         }
+        int result = Integer.parseInt(elements.pop());
+        int i = 1;
+        while (!elements.isEmpty()) {
+            if (elements.peek().equals("+")) {
+                elements.pop();
+                result += Integer.parseInt(elements.pop());
+            } else {
+                elements.pop();
+                result -= Integer.parseInt(elements.pop());
+            }
+            i++;
+        }
+        System.out.println(result);
     }
 }
