cef458c407ca5725e2d2ad0e7f45f1b2754269fe
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 13:59:42 +0300

    Create _01_BrowserHistory.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_01_BrowserHistory.java
@@ -0,0 +1,20 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _01_BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque <String> history = new ArrayDeque<>();
+        String url = scan.nextLine();
+        String currentUrl = "";
+        while (!url.equals("Home")){
+            if (url.equals("back")){
+                
+            }
+            url = scan.nextLine();
+        }
+
+    }
+}
