d5431cfbfbb0d4f8173973e41847a817cf8bb518
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 23:40:34 +0300

    Create _06_HotPotato.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_06_HotPotato.java
@@ -0,0 +1,23 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _06_HotPotato {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        Arrays.stream(scan.nextLine().split("\\s+")).forEach(queue::offer);
+        int number = Integer.parseInt(scan.nextLine());
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < number - 1; i++) {
+                String childToAdd = queue.poll();
+                queue.offer(childToAdd);
+            }
+            System.out.println("Removed " + queue.poll());
+        }
+        System.out.println("Last is " + queue.poll());
+    }
+}
