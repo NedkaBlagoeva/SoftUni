132136bd366c13a7153c6beeb089e47a4d768bd7
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 14:25:26 +0300

    Create _02_SimpleCalculator.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_02_SimpleCalculator.java
@@ -0,0 +1,15 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _02_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque <String> elements = new ArrayDeque<>();
+        String [] input = scan.nextLine().split("\\s+");
+        for (int i = input.length - 1; i >= 0 ; i--) {
+
+        }
+    }
+}
