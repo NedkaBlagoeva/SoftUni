f7b037bd43c9dcdfdcb961a88c11f531deee97d8
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 15:00:17 +0300

    Create _03_DecimalToBinary.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_03_DecimalToBinary.java
@@ -0,0 +1,10 @@
+package StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class _03_DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
