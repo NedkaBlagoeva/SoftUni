9c0a13518d1246b8ddaaac2a0f306d1bbe5f7e1a
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 14:25:19 +0300

    Update _01_BrowserHistory.java

--- JavaAdvanced/src/StacksAndQueues/Lab/_01_BrowserHistory.java
+++ JavaAdvanced/src/StacksAndQueues/Lab/_01_BrowserHistory.java
@@ -6,12 +6,22 @@ import java.util.Scanner;
 public class _01_BrowserHistory {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayDeque <String> history = new ArrayDeque<>();
+        ArrayDeque<String> history = new ArrayDeque<>();
         String url = scan.nextLine();
         String currentUrl = "";
-        while (!url.equals("Home")){
-            if (url.equals("back")){
-                
+        while (!url.equals("Home")) {
+            if (url.equals("back")) {
+                if (history.isEmpty() || history.size() == 1) {
+                    System.out.println("no previous URLs");
+                    url = scan.nextLine();
+                    continue;
+                } else {
+                    history.pop();
+                    System.out.println(history.peek());
+                }
+            } else {
+                history.push(url);
+                System.out.println(url);
             }
             url = scan.nextLine();
         }
