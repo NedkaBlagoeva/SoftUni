38752c79e5eddf1c6af255ed105c9a728cbb80c2
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 23:40:30 +0300

    Create _05_PrinterQueue.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_05_PrinterQueue.java
@@ -0,0 +1,29 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _05_PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        ArrayDeque<String> printQueue = new ArrayDeque<>();
+        while (!input.equals("print")) {
+            if (input.equals("cancel")) {
+                if (printQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                    input = scan.nextLine();
+                    continue;
+                } else {
+                    System.out.println("Canceled " + printQueue.poll());
+                }
+            } else {
+                printQueue.offer(input);
+            }
+            input = scan.nextLine();
+        }
+        while (!printQueue.isEmpty()) {
+            System.out.println(printQueue.poll());
+        }
+    }
+}
