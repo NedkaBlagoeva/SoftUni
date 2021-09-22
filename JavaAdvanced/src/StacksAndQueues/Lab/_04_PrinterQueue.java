ef6161e6ee93300f15885cd20af683f91e14bcba
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-22 16:37:31 +0300

    Create _04_PrinterQueue.java

---
+++ JavaAdvanced/src/StacksAndQueues/Lab/_04_PrinterQueue.java
@@ -0,0 +1,10 @@
+package StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class _04_PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
