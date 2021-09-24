d178101db57c3c136fb36fd2627ab648de3ece3b
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-24 14:30:05 +0300

    Update _03_MaximumElement.java

--- JavaAdvanced/src/StacksAndQueues/Exercise/_03_MaximumElement.java
+++ JavaAdvanced/src/StacksAndQueues/Exercise/_03_MaximumElement.java
@@ -1,10 +1,31 @@
 package StacksAndQueues.Exercise;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class _03_MaximumElement {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int n = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] commands = scan.nextLine().split("\\s+");
+            String command = commands[0];
+            switch (command) {
+                case "1":
+                    int number = Integer.parseInt(commands[1]);
+                    stack.push(number);
+                    break;
+                case "2":
+                    stack.pop();
+                    break;
+                case "3":
+                    System.out.println(Collections.max(stack));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
