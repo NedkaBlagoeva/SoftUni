1660a5b9850cfc34a612f037fcdcae4dedd25b33
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-24 14:18:02 +0300

    Create _01_ReverseNumbersWithAStack.java

---
+++ JavaAdvanced/src/StacksAndQueues/Exercise/_01_ReverseNumbersWithAStack.java
@@ -0,0 +1,16 @@
+package StacksAndQueues.Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _01_ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+        Arrays.stream(scan.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).forEach(numbers::push);
+
+        numbers.forEach(e -> System.out.print(e + " "));
+    }
+}
