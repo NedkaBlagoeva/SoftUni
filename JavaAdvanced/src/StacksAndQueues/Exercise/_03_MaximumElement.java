ac21dfa361d239dab2e899aa12aba6be15623bbe
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-24 14:18:09 +0300

    Create _03_MaximumElement.java

---
+++ JavaAdvanced/src/StacksAndQueues/Exercise/_03_MaximumElement.java
@@ -0,0 +1,10 @@
+package StacksAndQueues.Exercise;
+
+import java.util.Scanner;
+
+public class _03_MaximumElement {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
