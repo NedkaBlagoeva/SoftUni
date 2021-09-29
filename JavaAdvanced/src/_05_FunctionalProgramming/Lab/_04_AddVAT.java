e8afd8f6d70ca531ab8e76338cfef0277ad51b42
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-29 23:36:45 +0300

    Update _04_AddVAT.java

--- JavaAdvanced/src/_05_FunctionalProgramming/Lab/_04_AddVAT.java
+++ JavaAdvanced/src/_05_FunctionalProgramming/Lab/_04_AddVAT.java
@@ -1,10 +1,20 @@
 package _05_FunctionalProgramming.Lab;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 public class _04_AddVAT {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        List<Double> input = Arrays
+                .stream(scan.nextLine().split(", "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+        UnaryOperator<Double> vatCalculator = price -> price * 1.2;
+        System.out.println("Prices with VAT:");
+        input.stream().map(vatCalculator).forEach(e -> System.out.printf("%.2f%n", e));
     }
 }
