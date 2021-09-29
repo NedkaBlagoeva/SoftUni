4524956326c01f73f300da575b9832aac250beef
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-29 21:30:04 +0300

    Create _01_ReadFile.java

---
+++ JavaAdvanced/src/_04_StreamsFilesAndDirectories/Lab/_01_ReadFile.java
@@ -0,0 +1,12 @@
+package _04_StreamsFilesAndDirectories.Lab;
+
+import java.util.Scanner;
+
+public class _01_ReadFile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String path = "D:\\SoftUni\\JavaAdvanced\\src" +
+                "\\_04_StreamsFilesAndDirectories\\Lab\\" +
+                "04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+    }
+}
