ab9ae568f6d6edfc7cffcbc0d31f3ba62ac75cb7
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-29 21:29:59 +0300

    Create _04_AddVAT.java

---
+++ JavaAdvanced/src/_05_FunctionalProgramming/Lab/_04_AddVAT.java
@@ -0,0 +1,10 @@
+package _05_FunctionalProgramming.Lab;
+
+import java.util.Scanner;
+
+public class _04_AddVAT {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
