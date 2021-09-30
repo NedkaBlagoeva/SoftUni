83464a4da8e7916e0d9940e1b4688a958b4deb4f
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-30 16:02:50 +0300

    Create _06_PredicateForNames.java

---
+++ JavaAdvanced/src/_05_FunctionalProgramming/Exercise/_06_PredicateForNames.java
@@ -0,0 +1,19 @@
+package _05_FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class _06_PredicateForNames {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int length = Integer.parseInt(scan.nextLine());
+        List<String> names = Arrays
+                .stream(scan.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+        Predicate<String> filter = str -> str.length() <= length;
+        names.stream().filter(filter).forEach(System.out::println);
+    }
+}
