26f59ff64ac581b4dd45b772b20e5da4f8928317
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-30 23:22:51 +0300

    Create _09_ListOfPredicates.java

---
+++ JavaAdvanced/src/_05_FunctionalProgramming/Exercise/_09_ListOfPredicates.java
@@ -0,0 +1,10 @@
+package _05_FunctionalProgramming.Exercise;
+
+import java.util.Scanner;
+
+public class _09_ListOfPredicates {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
