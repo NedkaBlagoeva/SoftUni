f3bd4145b14b50fea95daab0e60e2b5c0157e6f4
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-09-30 23:22:39 +0300

    Update _07_FindTheSmallestElement.java

--- JavaAdvanced/src/_05_FunctionalProgramming/Exercise/_07_FindTheSmallestElement.java
+++ JavaAdvanced/src/_05_FunctionalProgramming/Exercise/_07_FindTheSmallestElement.java
@@ -1,8 +1,10 @@
 package _05_FunctionalProgramming.Exercise;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class _07_FindTheSmallestElement {
@@ -12,6 +14,10 @@ public class _07_FindTheSmallestElement {
                 .stream(scan.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        
+        Function<List<Integer>, Integer> findIndexMin = list -> {
+            int minInt = Collections.min(numbers);
+            return list.lastIndexOf(minInt);
+        };
+        System.out.println(findIndexMin.apply(numbers));
     }
 }
