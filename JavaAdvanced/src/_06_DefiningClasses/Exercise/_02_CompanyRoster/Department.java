504dbd69e0e5f28fdef9d223157fd764bd76ad33
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-06 23:46:33 +0300

    Update Department.java

--- JavaAdvanced/src/_06_DefiningClasses/Exercise/_02_CompanyRoster/Department.java
+++ JavaAdvanced/src/_06_DefiningClasses/Exercise/_02_CompanyRoster/Department.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Department {
-    private String name;
-    private List<Employee> employees;
+    private final String name;
+    private final List<Employee> employees;
 
     public Department (String name){
         this.name = name;
