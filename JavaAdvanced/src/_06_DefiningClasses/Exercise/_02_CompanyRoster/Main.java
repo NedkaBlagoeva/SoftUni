e0b509795794670056bdfd29ddbd60f581619005
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-06 18:52:04 +0300

    Create Main.java

---
+++ JavaAdvanced/src/_06_DefiningClasses/Exercise/_02_CompanyRoster/Main.java
@@ -0,0 +1,10 @@
+package _06_DefiningClasses.Exercise._02_CompanyRoster;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
