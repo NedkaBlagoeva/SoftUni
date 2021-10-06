7bc2355a865ad4fcd4e81bc3a8d525e8455ffbae
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-06 23:38:59 +0300

    Update Employee.java

--- JavaAdvanced/src/_06_DefiningClasses/Exercise/_02_CompanyRoster/Employee.java
+++ JavaAdvanced/src/_06_DefiningClasses/Exercise/_02_CompanyRoster/Employee.java
@@ -15,6 +15,31 @@ public class Employee {
         this.position = position;
         this.department = department;
         this.email = "n/a";
-        this.age = 0;
+        this.age = -1;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this(name, salary, position, department);
+        this.email = email;
+        this.age = age;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email) {
+        this(name, salary, position, department);
+        this.email = email;
+    }
+
+    public Employee(String name, double salary, String position, String department, int age) {
+        this(name, salary, position, department);
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %s %d",name, salary, email, age);
     }
 }
