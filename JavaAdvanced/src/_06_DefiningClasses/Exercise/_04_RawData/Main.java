199c4115161ac9595b00e0ec6d314ed8119374f5
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-09 22:05:09 +0300

    Create Main.java

---
+++ JavaAdvanced/src/_06_DefiningClasses/Exercise/_04_RawData/Main.java
@@ -0,0 +1,10 @@
+package _06_DefiningClasses.Exercise._04_RawData;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
