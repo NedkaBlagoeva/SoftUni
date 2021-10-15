2398800d524cd9cef71d263c7071fa3ef4ccd2ea
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-15 14:51:39 +0300

    Create _01_ParkingLot.java

---
+++ JavaAdvanced/src/_03_SetsAndMapsAdvanced/Lab/_01_ParkingLot.java
@@ -0,0 +1,10 @@
+package _03_SetsAndMapsAdvanced.Lab;
+
+import java.util.Scanner;
+
+public class _01_ParkingLot {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
