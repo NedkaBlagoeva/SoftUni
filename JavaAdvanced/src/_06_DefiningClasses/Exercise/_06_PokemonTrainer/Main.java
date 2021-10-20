1ce1bbaef6354b30c91c632c5a2b8bf8fe69a3ef
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-20 22:51:45 +0300

    Update Main.java

--- JavaAdvanced/src/_06_DefiningClasses/Exercise/_06_PokemonTrainer/Main.java
+++ JavaAdvanced/src/_06_DefiningClasses/Exercise/_06_PokemonTrainer/Main.java
@@ -15,36 +15,23 @@ public class Main {
             String pokemonElement = tokens[2];
             int pokemonHealth = Integer.parseInt(tokens[3]);
             Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
-            trainers.putIfAbsent(trainerName, new Trainer(trainerName, new ArrayList<>()));
-            trainers.get(trainerName).getPokemonCollection().add(pokemon);
+            trainers.putIfAbsent(trainerName, new Trainer());
+            trainers.get(trainerName).addPokemon(pokemon);
             input = scan.nextLine();
         }
+
         String commandElement = scan.nextLine(); //"Fire", "Water", "Electricity"
         while (!commandElement.equals("End")) {
             for (Trainer trainer : trainers.values()) {
-
-            }
-            for (Map.Entry<String, Trainer> trainerEntry : trainers.entrySet()) {
-                List<Pokemon> pokemons = trainerEntry.getValue().getPokemonCollection();
-                for (Pokemon pokemon : pokemons) {
-                    if (pokemon.getElement().equals(commandElement)) {
-                        int trainerBadges = trainerEntry.getValue().getNumberOfBadges();
-                        trainerEntry.getValue().setNumberOfBadges(trainerBadges + 1);
-                    } else {
-                        int pokemonHealth = pokemon.getHealth();
-                        pokemon.setHealth(pokemonHealth - 10);
-                        if (pokemon.getHealth() <= 0) {
-                            pokemons.remove(pokemon);
-                        }
-                    }
-                }
+                trainer.increaseBadges(commandElement);
             }
             commandElement = scan.nextLine();
         }
+
         trainers
                 .entrySet()
                 .stream()
                 .sorted((t1, t2) -> Integer.compare(t2.getValue().getNumberOfBadges(), t1.getValue().getNumberOfBadges()))
-                .forEach(t -> System.out.printf("%s %d %d%n", t.getValue().getName(), t.getValue().getNumberOfBadges(), t.getValue().getListSize()));
+                .forEach(t -> System.out.printf("%s %d %d%n", t.getKey(), t.getValue().getNumberOfBadges(), t.getValue().getListSize()));
     }
 }
