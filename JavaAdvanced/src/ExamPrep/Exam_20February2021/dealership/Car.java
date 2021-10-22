4d3a779a0cb48e911b8a888728e1eded901791f4
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-22 14:11:07 +0300

    Create Car.java

---
+++ JavaAdvanced/src/ExamPrep/Exam_20February2021/dealership/Car.java
@@ -0,0 +1,42 @@
+package ExamPrep.Exam_20February2021.dealership;
+
+public class Car {
+    public String manufacturer;
+    public String model;
+    public int year;
+
+    public Car(String manufacturer, String model, int year) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s (%d)", manufacturer, model, year);
+    }
+}
