f804c4b09ed5b629a98956c895d34555cdc96e54
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-22 16:12:35 +0300

    Update _02_Python.java

--- JavaAdvanced/src/ExamPrep/Exam_26June2021/_02_Python.java
+++ JavaAdvanced/src/ExamPrep/Exam_26June2021/_02_Python.java
@@ -1,4 +1,123 @@
 package ExamPrep.Exam_26June2021;
 
+import java.util.Scanner;
+
 public class _02_Python {
+    static int length = 1;
+    static int food = 0;
+    static int sRow;
+    static int sCol;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int screenSize = Integer.parseInt(scan.nextLine());
+        String[] commands = scan.nextLine().split(", ");
+        char[][] field = new char[screenSize][screenSize];
+
+        for (int r = 0; r < screenSize; r++) {
+            String line = scan.nextLine().replaceAll("\\s+", "");
+            field[r] = line.toCharArray();
+
+            if (line.contains("s")) {
+                sRow = r;
+                sCol = line.indexOf("s");
+            }
+
+            for (char c : field[r]) {
+                if (c == 'f') {
+                    food++;
+                }
+            }
+        }
+        for (String command : commands) {
+            switch (command) {
+                case "up": //row - 1
+                    sRow -= 1;
+                    if (isOutOfBounds(screenSize, sRow, sCol)) {
+                        sRow = screenSize - 1;
+                    }
+                    checkFood(field, sRow, sCol);
+                    if (foodWin()) {
+                        return;
+                    }
+                    if (enemyKill(field, sRow, sCol)) {
+                        return;
+                    }
+                    break;
+                case "down": //row + 1
+                    sRow += 1;
+                    if (isOutOfBounds(screenSize, sRow, sCol)) {
+                        sRow = 0;
+                    }
+                    checkFood(field, sRow, sCol);
+                    if (foodWin()) {
+                        return;
+                    }
+                    if (enemyKill(field, sRow, sCol)) {
+                        return;
+                    }
+                    break;
+                case "right": //col + 1
+                    sCol += 1;
+                    if (isOutOfBounds(screenSize, sRow, sCol)) {
+                        sCol = 0;
+                    }
+                    checkFood(field, sRow, sCol);
+                    if (foodWin()) {
+                        return;
+                    }
+                    if (enemyKill(field, sRow, sCol)) {
+                        return;
+                    }
+                    break;
+                case "left": //col - 1
+                    sCol -= 1;
+                    if (isOutOfBounds(screenSize, sRow, sCol)) {
+                        sCol = screenSize - 1;
+                    }
+                    checkFood(field, sRow, sCol);
+                    if (foodWin()) {
+                        return;
+                    }
+                    if (enemyKill(field, sRow, sCol)) {
+                        return;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.printf("You lose! There is still %d food to be eaten.", food);
+    }
+
+    private static boolean enemyKill(char[][] matrix, int sRow, int sCol) {
+        char currentPosition = matrix[sRow][sCol];
+        if (currentPosition == 'e') {
+            System.out.println("You lose! Killed by an enemy!");
+            return true;
+        }
+        return false;
+    }
+
+    private static void checkFood(char[][] matrix, int sRow, int sCol) {
+        char currentPosition = matrix[sRow][sCol];
+        if (currentPosition == 'f') {
+            food--;
+            length++;
+        }
+    }
+
+    private static boolean foodWin() {
+        if (food == 0) {
+            System.out.println("You win! Final python length is " + length);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isOutOfBounds(int size, int sRow, int sCol) {
+        return sRow >= size || sRow < 0 || sCol >= size || sCol < 0;
+    }
+
+
 }
