dc78957085085dd9032c20061bf7ae5fc7df17fb
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-22 16:23:29 +0300

    Create _02_Bomb.java

---
+++ JavaAdvanced/src/ExamPrep/Exam_20February2021/_02_Bomb.java
@@ -0,0 +1,9 @@
+package ExamPrep.Exam_20February2021;
+
+import java.util.Scanner;
+
+public class _02_Bomb {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
