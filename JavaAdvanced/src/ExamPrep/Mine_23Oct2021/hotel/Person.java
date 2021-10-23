e813c07217f4a8487734614d1e4bf348434405e1
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-10-23 13:46:17 +0300

    Create Person.java

---
+++ JavaAdvanced/src/ExamPrep/Mine_23Oct2021/hotel/Person.java
@@ -0,0 +1,52 @@
+package ExamPrep.Mine_23Oct2021.hotel;
+
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+    private String hometown = "n/a";
+
+    public Person(String name, int id, int age, String hometown) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person %s: %d, Age: %d, Hometown: %s", name, id, age, hometown);
+    }
+}
