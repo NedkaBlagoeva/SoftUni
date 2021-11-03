a28fb3c08ffb854d9ceb28ac10569772f693a9c4
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-11-03 15:39:15 +0200

    update

--- JavaOOP/src/_01_WorkingWithAbstraction/Lab/_01_RhombusOfStars.java
+++ JavaOOP/src/_01_WorkingWithAbstraction/Lab/_01_RhombusOfStars.java
@@ -1,8 +1,12 @@
 package _01_WorkingWithAbstraction.Lab;
 
+import java.util.Scanner;
+
 public class _01_RhombusOfStars {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
 
     }
 }
