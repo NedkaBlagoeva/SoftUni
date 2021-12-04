f0eec77f9bd363b7a3cbd2d5655d6841aa8fb802
NedkaBlagoeva <79089794+NedkaBlagoeva@users.noreply.github.com>
2021-12-04 16:03:02 +0200

    Task 2 Multiple Implementation

---
+++ JavaOOP/src/_04_InterfacesAndAbstraction/Exercise/_02_MultipleImplementation/Identifiable.java
@@ -0,0 +1,5 @@
+package _04_InterfacesAndAbstraction.Exercise._02_MultipleImplementation;
+
+public interface Identifiable {
+    String getId();
+}
---
+++ JavaOOP/src/_04_InterfacesAndAbstraction/Exercise/_02_MultipleImplementation/Main.java
@@ -0,0 +1,28 @@
+package _04_InterfacesAndAbstraction.Exercise._02_MultipleImplementation;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Class[] citizenInterfaces = Citizen.class.getInterfaces();
+        if (Arrays.asList(citizenInterfaces).contains(Birthable.class)
+                && Arrays.asList(citizenInterfaces).contains(Identifiable.class)) {
+            Method[] methods = Birthable.class.getDeclaredMethods();
+            methods = Identifiable.class.getDeclaredMethods();
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            int age = Integer.parseInt(scanner.nextLine());
+            String id = scanner.nextLine();
+            String birthDate = scanner.nextLine();
+            Identifiable identifiable = new Citizen(name, age, id, birthDate);
+            Birthable birthable = new Citizen(name, age, id, birthDate);
+            System.out.println(methods.length);
+            System.out.println(methods[0].getReturnType().getSimpleName());
+            System.out.println(methods.length);
+            System.out.println(methods[0].getReturnType().getSimpleName());
+        }
+    }
+
+}
