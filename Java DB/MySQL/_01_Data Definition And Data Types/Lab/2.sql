9ec813701e551064c57717310169f52793359ad4
NedkaBlagoeva <nedkablagoeva@gmail.com>
2022-01-20 14:46:07 +0200

    Rearranged

---
+++ Java DB/MySQL/_01_Data Definition And Data Types/Lab/2.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees 
+Values
+(1,'Ivan', 'Ivanov'),
+(2, 'Pesho', 'Peshev'),
+(3, 'Dryndulin', 'Penchev');
---
+++ Java DB/MySQL/_03_Build-In Functions/Lab/01.sql
@@ -0,0 +1,3 @@
+SELECT title FROM books
+WHERE title LIKE 'The%'
+ORDER BY id;
