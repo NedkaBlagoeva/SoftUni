6dbc9cc220b7e854d1f1c13f48d484d57f3b6129
NedkaBlagoeva <nedkablagoeva@gmail.com>
2022-02-10 20:01:47 +0200

    Subqueries and Joins Tasks Lab 1-4

---
+++ Java DB/MySQL/_06_Subqueries and JOINs/Lab/03.sql
@@ -0,0 +1,3 @@
+SELECT employee_id, first_name, last_name, department_id, salary 
+FROM employees
+WHERE manager_id IS NULL;
---
+++ Java DB/MySQL/_06_Subqueries and JOINs/Lab/04.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(*) FROM employees
+WHERE salary > (SELECT AVG(salary) FROM employees);
